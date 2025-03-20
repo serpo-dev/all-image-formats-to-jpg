cc750d51048c4a4d2719bfcf3877ce3f19e4be87
serpo-dev <sergey.potapov.2002@mail.ru>
2025-03-20 12:35:42 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Usage
+
+Put your images into `input/` folder, then run the script `$ python converter.py`.
+
+Check out the result in `output/` folder.
+
+## Supported formats
+
+`PNG`, `GIF`, `TIFF`, `BMP`, `PDF`, `PCX`, `TGA`, `EPS`, `PSD`, `ICO`, `PPM`, `PBM`, `PGM`, `XCF`, `APNG`, `AVIF`, `HDR`, `JFIF`, `HEIC`
---
+++ convert.py
@@ -0,0 +1,60 @@
+import os
+from PIL import Image, ImageOps
+import time
+import pillow_heif
+
+input_folder = 'input'
+output_folder = 'output'
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+supported_formats = ['JPEG', 'JPG', 'PNG', 'GIF', 'TIFF', 'BMP', 'PDF', 'PCX', 'TGA', 'EPS', 'PSD', 'ICO', 'PPM', 'PBM', 'PGM', 'XCF', 'APNG', 'AVIF', 'HDR', 'JFIF', 'HEIC']
+files = [f for f in os.listdir(input_folder) if any(f.upper().endswith(fmt) for fmt in supported_formats)]
+total_files = len(files)
+start_time = time.time()
+
+def correct_orientation(img):
+    try:
+        if hasattr(img, '_getexif'):
+            exif = img._getexif()
+            if exif is not None:
+                orientation = exif.get(274)
+                if orientation == 3:
+                    img = img.rotate(180, expand=True)
+                elif orientation == 6:
+                    img = img.rotate(270, expand=True)
+                elif orientation == 8:
+                    img = img.rotate(90, expand=True)
+    except Exception as e:
+        print(f"EXIF processing error: {e}")
+    return img
+
+for i, file in enumerate(files):
+    file_path = os.path.join(input_folder, file)
+    output_path = os.path.join(output_folder, os.path.splitext(file)[0] + '.jpg')
+    
+    try:
+        if not os.path.isfile(file_path):
+            print(f"Error: {file} is not a valid file. Skipping.")
+            continue
+
+        if file.upper().endswith('.HEIC'):
+            heif_file = pillow_heif.open_heif(file_path)
+            img = Image.frombytes(heif_file.mode, heif_file.size, heif_file.data, "raw")
+        else:
+            with Image.open(file_path) as img:
+                img = correct_orientation(img)
+                img = img.convert('RGB')
+            
+        img.save(output_path, 'JPEG')
+        
+        elapsed_time = time.time() - start_time
+        avg_time_per_file = elapsed_time / (i + 1)
+        remaining_time = avg_time_per_file * (total_files - i - 1)
+        
+        print(f'Processed: {i + 1}/{total_files}, Estimated time remaining: {remaining_time:.2f} sec')
+    except Exception as e:
+        print(f'Error processing file {file}: {str(e)}')
+
+print('Conversion completed.')
